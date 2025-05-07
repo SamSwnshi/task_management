3e377a8addf4e87d12d47c7b79a3603275dd361c
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-05-07 22:12:36 +0530

    task model

---
+++ backend/models/task.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    status: {
+      type: String,
+      enum: ["incomplete", "complete"],
+      default: "incomplete",
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    createdAt: { type: Date, default: Date.now },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task",taskSchema);
+
+export default Task;

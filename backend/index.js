d30e825458eb63615893b5f7e0d9cf98ab75f9ed
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-05-08 16:37:59 +0530

    minor tweak

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "*", 
     credentials: true
 }));
 app.use(express.json())
